6da223025e30fc6bd8dd40dc1f5396b9b62d7192
parassareen4 <kyakrnahaibro@gmail.com>
2024-11-15 15:37:28 -0500

    cool

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,33 @@ const users = [
         username: "home-depot.bsky.social",
         password: "parassareen1",
         sessionToken: "eyJ0eXAiOiJhdCtqd3QiLCJhbGciOiJFUzI1NksifQ.eyJzY29wZSI6ImNvbS5hdHByb3RvLmFjY2VzcyIsInN1YiI6ImRpZDpwbGM6MjNscXplbTVrNW5iYTd2NnQ1c3JldWo1IiwiaWF0IjoxNzMxNzAxMTgxLCJleHAiOjE3MzE3MDgzODEsImF1ZCI6ImRpZDp3ZWI6c2NhcmxldGluYS51cy1lYXN0Lmhvc3QuYnNreS5uZXR3b3JrIn0.740xou4LHNAwsWJ-B290NNm5cOQuyXlbtbmiHR81O9rRpqUJM7UkidYASEfDkBzF7bPN0tLZx2LePi7KEi4rhw" // Empty token will be populated later
+    },
+    {
+        username: "victorias-secret.bsky.social",
+        password: "parassareen1",
+        sessionToken: "eyJ0eXAiOiJhdCtqd3QiLCJhbGciOiJFUzI1NksifQ.eyJzY29wZSI6ImNvbS5hdHByb3RvLmFjY2VzcyIsInN1YiI6ImRpZDpwbGM6cmYyeDVnZzRqM3VicWljMjdrZ3pod2Y1IiwiaWF0IjoxNzMxNzAyMDMzLCJleHAiOjE3MzE3MDkyMzMsImF1ZCI6ImRpZDp3ZWI6cmVpc2hpLnVzLWVhc3QuaG9zdC5ic2t5Lm5ldHdvcmsifQ.uXGz36m4w9IuVWpw6xHgQdfr6oQF-zi66GxfqSv1--Vjxs3PhSgKySnKfsqPokkiLUzN2FKyu9iLZnAdGsBW9Q" // Empty token will be populated later
+    },{
+        username: "pix11news.bsky.social",
+        password: "parassareen1",
+        sessionToken: "eyJ0eXAiOiJhdCtqd3QiLCJhbGciOiJFUzI1NksifQ.eyJzY29wZSI6ImNvbS5hdHByb3RvLmFjY2VzcyIsInN1YiI6ImRpZDpwbGM6cGk2ZG1ydWtrcWFna2U1eW93aDdoYm53IiwiaWF0IjoxNzMxNzAyMjA0LCJleHAiOjE3MzE3MDk0MDQsImF1ZCI6ImRpZDp3ZWI6bWVhZG93LnVzLWVhc3QuaG9zdC5ic2t5Lm5ldHdvcmsifQ.yrwMZKoymJKKjpQpq00H4XNZf0WXpYmasRRvsXGgByzquzCLxb0ngBqBy8ha2jCCkYBBkv-Yb7D5HcmeztjxOQ" // Empty token will be populated later
+    },{
+        username: "ben-n-jerrys.bsky.social",
+        password: "parassareen1",
+        sessionToken: "eyJ0eXAiOiJhdCtqd3QiLCJhbGciOiJFUzI1NksifQ.eyJzY29wZSI6ImNvbS5hdHByb3RvLmFjY2VzcyIsInN1YiI6ImRpZDpwbGM6dTVoYnJ3dmhsZjJkYWozNXg1MnhpdWVhIiwiaWF0IjoxNzMxNzAyMzQ3LCJleHAiOjE3MzE3MDk1NDcsImF1ZCI6ImRpZDp3ZWI6aGVsdmVsbGEudXMtZWFzdC5ob3N0LmJza3kubmV0d29yayJ9.3huLVKvnQuaZOLH92IV9U15Ann3NT9SemGa3LMSsdv_uDNFptM129U1_M-b0prPful8rGZyKGQIt-BGdsedpiw" // Empty token will be populated later
+    },{
+        username: "delta-air-lines.bsky.social",
+        password: "parassareen1",
+        sessionToken: "eyJ0eXAiOiJhdCtqd3QiLCJhbGciOiJFUzI1NksifQ.eyJzY29wZSI6ImNvbS5hdHByb3RvLmFjY2VzcyIsInN1YiI6ImRpZDpwbGM6eDI0bzdmcGxubm11dDRhM3A0ZHhvdG14IiwiaWF0IjoxNzMxNzAyNjAzLCJleHAiOjE3MzE3MDk4MDMsImF1ZCI6ImRpZDp3ZWI6d2l0Y2hlc2J1dHRlci51cy13ZXN0Lmhvc3QuYnNreS5uZXR3b3JrIn0.mWTiYiV8XxyWDVxry9RLP5kxUedlwp6beedclu9H37q6I3Icm5cW12HjsXZu31SQ3YakZHpao2U_keoLEEAqkA" // Empty token will be populated later
+    },{
+        username: "time-magazine.bsky.social",
+        password: "parassareen1",
+        sessionToken: "eyJ0eXAiOiJhdCtqd3QiLCJhbGciOiJFUzI1NksifQ.eyJzY29wZSI6ImNvbS5hdHByb3RvLmFjY2VzcyIsInN1YiI6ImRpZDpwbGM6c2xydGlxNXZqN2l5cTN4NHN3NXd1YTJoIiwiaWF0IjoxNzMxNzAyNzcyLCJleHAiOjE3MzE3MDk5NzIsImF1ZCI6ImRpZDp3ZWI6cGFyYXNvbC51cy1lYXN0Lmhvc3QuYnNreS5uZXR3b3JrIn0.99XtBk1NS8gs8g8TA9-UddeHTNyfSVh4kgIH0DDGoQ9ooLUSlcu_4mEt4cToYiYd5a_GSKLDMD7H8DE1YffzRA" // Empty token will be populated later
+    },{
+        username: "buzzfeeds-news.bsky.social",
+        password: "parassareen1",
+        sessionToken: "eyJ0eXAiOiJhdCtqd3QiLCJhbGciOiJFUzI1NksifQ.eyJzY29wZSI6ImNvbS5hdHByb3RvLmFjY2VzcyIsInN1YiI6ImRpZDpwbGM6aXVmNTdsNHo1bWk0cGlsanV1Y3d1empoIiwiaWF0IjoxNzMxNzAyODk2LCJleHAiOjE3MzE3MTAwOTYsImF1ZCI6ImRpZDp3ZWI6Z2Fub2Rlcm1hLnVzLXdlc3QuaG9zdC5ic2t5Lm5ldHdvcmsifQ.L7-f8gNtVIUI7TyowlISNjqb9kmjXKGsoD8E6nUAnXZTRoA10cZRfUi4pQwrMWV3-KO8Hz5LZpG1SxjiOVwgAQ" // Empty token will be populated later
     }
+
 ];
 
 // Middleware for logging
